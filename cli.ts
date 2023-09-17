8ef5d5566d9d330033123bbb3eb5ec367e26ddb2
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-09-17 15:59:45 +0800

    populating package.json fields

--- cli.ts
+++ cli.ts
@@ -7,22 +7,50 @@ import { cwd } from 'process'
 
 function main() {
   let pkg = readPackageJSON()
-  let { entryFile, globalName } = parseArgs(process.argv.slice(2), pkg)
-  pkg.main ||= 'dist/cjs.js'
+  let { globalName, entryFile, testFile, outDir } = parseArgs(
+    process.argv.slice(2),
+    pkg,
+  )
+  pkg.main ||= `${outDir}/cjs.js`
+  pkg.types ||= `${outDir}/${basename(entryFile).replace(/\.ts$/, '.d.ts')}`
+  pkg.module ||= `${outDir}/esm.js`
+  pkg.browser ||= `${outDir}/browser.js`
+  pkg.unpkg ||= pkg.browser
+  pkg.files ||= []
+  if (!pkg.files.includes(outDir)) {
+    pkg.files.push(outDir)
+  }
+  pkg.scripts ||= {}
+  pkg.scripts.test ||= `ts-mocha ${testFile}`
+  pkg.scripts.coverage ||= 'nyc npm test'
+  pkg.scripts.build ||= 'run-s clean transpile'
+  pkg.scripts.clean ||= 'rimraf dist'
+  pkg.scripts.transpile ||= 'run-p esbuild tsc'
+  pkg.scripts.esbuild ||= 'node scripts/esbuild.js'
+  pkg.scripts.tsc ||= 'tsc -p tsconfig.esbuild.json'
 }
 
 function parseArgs(args: string[], pkg: pkg) {
   let entryFile = ''
   let globalName = ''
+  let outDir = 'dist'
 
   for (let i = 0; i < args.length; i++) {
     let arg = args[i]
     if (arg.startsWith('--entryFile')) {
-      entryFile = arg.split('=')[1]
+      entryFile = parseArgValue(arg)
       continue
     }
-    if (arg.startsWith('--name')) {
-      globalName = arg.split('=')[1]
+    if (
+      arg.startsWith('--name') ||
+      arg.startsWith('--global') ||
+      arg.startsWith('--globalName')
+    ) {
+      globalName = parseArgValue(arg)
+      continue
+    }
+    if (arg.startsWith('--outDir')) {
+      outDir = parseArgValue(arg)
       continue
     }
     console.error('Unknown argument: ' + JSON.stringify(arg))
@@ -48,7 +76,20 @@ function parseArgs(args: string[], pkg: pkg) {
     `src/${packageName}`,
   ])
 
-  return { globalName, entryFile }
+  let testFile = detectFile([
+    entryFile.replace(/\.ts$/, '.spec.ts'),
+    entryFile.replace(/\.ts$/, '.test.ts'),
+  ])
+
+  return { globalName, entryFile, testFile, outDir }
+}
+
+function parseArgValue(arg: string): string {
+  let value = arg.split('=')[1]
+  if (value) return value
+  console.error('Missing argument value for: ' + JSON.stringify(arg))
+  console.error('Example: ' + arg.replace('=', '') + '=value')
+  process.exit(1)
 }
 
 function detectFile(files: string[]): string {
@@ -70,6 +111,20 @@ function readPackageJSON(): pkg {
 type pkg = Partial<{
   name: string
   main: string
+  types: string
+  module: string
+  browser: string
+  unpkg: string
+  files: string[]
+  scripts: Partial<{
+    test: string
+    coverage: string
+    build: string
+    clean: string
+    transpile: string
+    esbuild: string
+    tsc: string
+  }>
 }>
 
 function getPackageName(pkg: pkg): string {
